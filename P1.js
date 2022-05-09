b21c433780b9c55749f4e3ffe2f724bb8eafc33f
Aroskar17 <93914017+Aroskar17@users.noreply.github.com>
2022-05-09 17:58:00 +0530

    Create P1.js

---
+++ P1.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+const JSX =<div>
+<h1>HelloWorld</h1>
+<h2>React Js id live</h2>
+</div>;
+ReactDOM.render(JSX,document.getElementById('root'));
